e94c0325d344943741cc8a0d9f10eef482af9f58
MC_XiaoHei <xor7xiaohei@gmail.com>
2025-12-24 22:09:33 +0800

    refactor: better arguments parse

--- src-tauri/src/launcher/aruments.rs
+++ src-tauri/src/launcher/aruments.rs
@@ -8,7 +8,7 @@ impl Arguments {
         context: RuleContext,
         launch_args: HashMap<String, String>,
     ) -> Vec<String> {
-        Self::get_arguments(self.game.clone(), context, launch_args)
+        Self::get_arguments(self.game.clone(), context, &launch_args)
     }
 
     pub fn get_jvm_arguments(
@@ -16,28 +16,32 @@ impl Arguments {
         context: RuleContext,
         launch_args: HashMap<String, String>,
     ) -> Vec<String> {
-        Self::get_arguments(self.jvm.clone(), context, launch_args)
+        Self::get_arguments(self.jvm.clone(), context, &launch_args)
     }
 
     fn get_arguments(
         args: Vec<ArgumentValue>,
         context: RuleContext,
-        launch_args: HashMap<String, String>,
+        launch_args: &HashMap<String, String>,
     ) -> Vec<String> {
-        let mut result = vec![];
-        for arg in args {
-            let mut value = arg.get_value(context.clone());
-            for v in value.iter_mut() {
-                for (key, val) in launch_args.iter() {
-                    if v == format!("${{{}}}", key).as_str() {
-                        *v = val.to_string();
-                        break;
-                    }
-                }
+        args.iter()
+            .fold(vec![], |mut acc, arg| {
+                acc.extend(arg.get_value(context.clone()));
+                acc
+            })
+            .iter()
+            .map(|arg| Self::replace_placeholder(arg.into(), launch_args))
+            .collect()
+    }
+
+    fn replace_placeholder(arg: String, launch_args: &HashMap<String, String>) -> String {
+        for (key, value) in launch_args {
+            let placeholder = format!("${{{key}}}");
+            if arg == placeholder {
+                return value.into();
             }
-            result.extend(value);
         }
-        result
+        arg
     }
 }
 
@@ -53,5 +57,32 @@ impl ArgumentValue {
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+#[cfg_attr(coverage_nightly, coverage(off))]
+mod tests {
+    use crate::resolver::model::Arguments;
+    use std::collections::HashMap;
+
+    #[test]
+    fn test_placeholder_replacement() {
+        let value = "test_value".to_string();
+        let launch_args = HashMap::from([("placeholder".to_string(), value.clone())]);
+
+        let result = Arguments::replace_placeholder("${placeholder}".into(), &launch_args);
+        assert_eq!(
+            result,
+            value.clone(),
+            "Placeholder should be replaced with the correct value"
+        );
+
+        let no_replace = "no_replacement_needed";
+        let result_no_replace = Arguments::replace_placeholder(no_replace.into(), &launch_args);
+        assert_eq!(
+            result_no_replace, no_replace,
+            "String without placeholder should remain unchanged"
+        );
     }
 }
