4b02d4d06c06d81d2fc9d3f73517d70986bf525a
MC_XiaoHei <xor7xiaohei@gmail.com>
2026-01-11 21:57:50 +0800

    feat: add shadcn & tailwind

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue"
 import { createPinia } from "pinia"
 import App from "./App.vue"
 import { attachConsole } from "@tauri-apps/plugin-log"
+import "./style.css"
 
 await attachConsole()
 
