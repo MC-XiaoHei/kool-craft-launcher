39a613fc2fa4908da6c57c59ec39289b2e290b7b
MC_XiaoHei <xor7xiaohei@gmail.com>
2026-01-12 17:30:09 +0800

    feat: add window buttons

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
   import { cacheWallpaper, getCachedWallpaper } from "@/composables/wallpaper"
   import { ThemeEffect } from "@/bindings/types"
   import { config } from "@/services/backend/config"
+  import { Button } from "@/components/ui/button"
+  import { PhMinus, PhX } from "@phosphor-icons/vue"
 
   const appWindow = getCurrentWindow()
   const wallpaperUrl = ref(getCachedWallpaper())
@@ -54,6 +56,14 @@
       loadWallpaper().then()
     }
   }
+
+  async function minimizeWindow() {
+    await appWindow.minimize()
+  }
+
+  async function closeWindow() {
+    await appWindow.close()
+  }
 </script>
 
 <template>
@@ -75,6 +85,24 @@
       <div
         class="absolute inset-0 transition-colors duration-300 bg-[#f3f3f3]/80 mix-blend-overlay dark:bg-[#202020]/85 dark:mix-blend-normal"
       ></div>
+    </div>
+
+    <div class="absolute right-1 z-100 flex items-center">
+      <Button
+        variant="ghost"
+        class="size-10 no-drag hover:bg-transparent text-muted-foreground hover:text-foreground"
+        @click="minimizeWindow"
+      >
+        <PhMinus class="h-5 w-5" weight="bold" />
+      </Button>
+
+      <Button
+        variant="ghost"
+        class="size-10 no-drag hover:bg-transparent text-muted-foreground hover:text-red-600 dark:hover:text-red-500"
+        @click="closeWindow"
+      >
+        <PhX class="h-5 w-5" weight="bold" />
+      </Button>
     </div>
 
     <main class="relative z-10 h-full flex items-center justify-center p-10" data-tauri-drag-region>
