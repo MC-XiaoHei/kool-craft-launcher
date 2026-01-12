b72d9e5d0d48f815ed2bd0f33343b1c489e522b2
MC_XiaoHei <xor7xiaohei@gmail.com>
2026-01-12 19:55:07 +0800

    fix: fix sync-metadata always cause rebuild

--- sync-metadata.cjs
+++ sync-metadata.cjs
@@ -3,33 +3,39 @@ const { name, version, identifier } = readPackageJson()
 updateTauriConfig()
 updateCargoToml()
 
-console.log('Tauri config updated with package.json values.')
+console.log("Tauri config updated with package.json values.")
 
 function readPackageJson() {
-  const packageJsonFile = resolve('package.json')
+  const packageJsonFile = resolve("package.json")
   const packageJson = JSON.parse(readFile(packageJsonFile))
 
   return {
     name: packageJson.name,
     version: packageJson.version,
-    identifier: packageJson.identifier
+    identifier: packageJson.identifier,
   }
 }
 
 function updateTauriConfig() {
-  const tauriConfigFile = resolve('src-tauri/tauri.conf.json')
+  const tauriConfigFile = resolve("src-tauri/tauri.conf.json")
   const tauriConfig = JSON.parse(readFile(tauriConfigFile))
 
-  tauriConfig.productName = name
-  tauriConfig.version = version
-  tauriConfig.identifier = identifier
+  if (
+    tauriConfig.productName !== name ||
+    tauriConfig.version !== version ||
+    tauriConfig.identifier !== identifier
+  ) {
+    tauriConfig.productName = name
+    tauriConfig.version = version
+    tauriConfig.identifier = identifier
 
-  writeFile(tauriConfigFile, JSON.stringify(tauriConfig, null, 2) + '\n')
+    writeFile(tauriConfigFile, JSON.stringify(tauriConfig, null, 2) + "\n")
+  }
 }
 
 function updateCargoToml() {
-  const cargoTomlFile = resolve('src-tauri/Cargo.toml')
-  const cargoToml = readFile(cargoTomlFile, 'utf8')
+  const cargoTomlFile = resolve("src-tauri/Cargo.toml")
+  const cargoToml = readFile(cargoTomlFile, "utf8")
 
   const updatedCargoToml = cargoToml
     .replace(/^name\s*=\s*".*"/m, `name = "${name}"`)
@@ -39,13 +45,13 @@ function updateCargoToml() {
 }
 
 function resolve(file) {
-  return require('path').resolve(__dirname, file)
+  return require("path").resolve(__dirname, file)
 }
 
 function readFile(path) {
-  return require('fs').readFileSync(path, 'utf8')
+  return require("fs").readFileSync(path, "utf8")
 }
 
 function writeFile(path, content) {
-  require('fs').writeFileSync(path, content)
+  require("fs").writeFileSync(path, content)
 }
