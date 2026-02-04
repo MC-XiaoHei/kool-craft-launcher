e7c37fd8ae9a2dff5efcc7cb33f06a889e85e705
MC_XiaoHei <xor7xiaohei@gmail.com>
2026-02-04 11:22:14 +0800

    feat: auto inject env in nv + wayland system

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,10 +3,14 @@
 #![cfg_attr(coverage_nightly, feature(coverage_attribute))]
 #![cfg_attr(coverage_nightly, coverage(off))]
 
+use std::env;
+
 fn main() {
     #[cfg(target_os = "windows")]
     #[cfg(debug_assertions)]
-    configure_dev_webview_runtime();
+    try_configure_dev_webview_runtime();
+    #[cfg(target_os = "linux")]
+    try_resolve_nv_wayland_issue();
     kool_craft_launcher_lib::run().unwrap_or_else(log_error);
 }
 
@@ -16,8 +20,8 @@ fn log_error(err: anyhow::Error) {
 
 #[cfg(target_os = "windows")]
 #[cfg(debug_assertions)]
-fn configure_dev_webview_runtime() {
-    let _ = std::env::current_exe()
+fn try_configure_dev_webview_runtime() {
+    let _ = env::current_exe()
         .ok()
         .and_then(|path| path.parent().map(|p| p.join("runtime")))
         .filter(|path| path.is_dir())
@@ -27,7 +31,26 @@ fn configure_dev_webview_runtime() {
             // 1. Concurrency: This runs at the start of main before any threads are spawned, avoiding race conditions.
             // 2. Build Scope: This function is guarded by #[cfg(debug_assertions)], so this unsafe code is completely stripped from release builds and poses no risk to production.
             unsafe {
-                std::env::set_var("WEBVIEW2_BROWSER_EXECUTABLE_FOLDER", path);
+                env::set_var("WEBVIEW2_BROWSER_EXECUTABLE_FOLDER", path);
             }
         });
+}
+
+// Related issue: https://github.com/tauri-apps/tauri/issues/10702
+#[cfg(target_os = "linux")]
+fn try_resolve_nv_wayland_issue() {
+    let is_wayland = env::var("XDG_SESSION_TYPE")
+        .map(|v| v.to_lowercase().contains("wayland"))
+        .unwrap_or(false);
+
+    let has_nvidia = std::path::Path::new("/proc/driver/nvidia").exists();
+
+    if is_wayland && has_nvidia {
+        println!("Nvidia & Wayland detected, disabling Explicit Sync & Threaded Optimizations");
+        // SAFETY: This runs at the start of main before any threads are spawned, avoiding race conditions.
+        unsafe {
+            env::set_var("__NV_DISABLE_EXPLICIT_SYNC", "1");
+            env::set_var("__GL_THREADED_OPTIMIZATIONS", "0");
+        }
+    }
 }
